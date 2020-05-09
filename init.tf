387f3709346be11355867b0b00a41d6eff3a1ad6
solomson <auls@equinor.com>
2020-05-09 13:57:14 +0200

    init: create backend state storage

--- init.tf
+++ init.tf
@@ -1,10 +1,36 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "sdp-common-infrastructure"
+    storage_account_name = "sdpterraform"
+    container_name       = "tfstate-prod"
+    key                  = "tfstate"
+  }
+}
+
 provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x. 
-  # If you are using version 1.x, the "features" block is not allowed.
   version = "~>2.0"
   features {}
 }
+
 resource "azurerm_resource_group" "rg" {
-        name = "rg1"
+        name = "sdp-common-infrastructure"
         location = "norwayeast"
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "sdpterraform"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate-prod" {
+  name                  = "tfstate-prod"
+  storage_account_name  = azurerm_storage_account.storageaccount.name
+  container_access_type = "private"
 }
